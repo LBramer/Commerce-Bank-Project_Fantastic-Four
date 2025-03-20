e451c23233f1009284c651554ab95a15a6a08907
TDMartin66 <tdm70080@ucmo.edu>
2025-03-20 17:58:30 -0500

    Add files via upload

---
+++ commerceBankDB.sql
@@ -0,0 +1,25 @@
+create database commerceBank;
+use commerceBank;
+select database();
+
+CREATE TABLE User
+(username VARCHAR(30),
+password VARCHAR(30),
+PRIMARY KEY (username));
+
+CREATE TABLE URL_Table
+(URL_Index INT NOT NULL,
+URL_str  VARCHAR(256),
+PRIMARY KEY (URL_Index));
+
+CREATE TABLE URL_DATA
+(URL_str VARCHAR(256),
+expiration VARCHAR(256),
+name VARCHAR(30),
+responseCode VARCHAR(100),
+responseHeaders VARCHAR(100),
+SSLProtocol VARCHAR(30),
+PRIMARY KEY (URL_str),
+FOREIGN KEY (URL_Index) REFERENCES URL_Table(URL_Index));
+
+
